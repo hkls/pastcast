705f107f94891ab25f608bad28f02378407d8cd6
Wynand Meintjes <wynandmeintjes@users.noreply.github.com>
2025-07-21 22:44:05 +0200

    feat: Adds the weatherstack lookup (#6)
    * feat: Adds the weatherstack lookup
    
    * fix: Weatherstack deserialize needs some TLC

--- src/configs/config.rs
+++ src/configs/config.rs
@@ -1,15 +1,35 @@
 use std::env;
 
-pub struct Config {
+pub struct EnvConfig {
+    pub db_spec: DBSpec,
+    pub weatherstack_spec: WeatherStackSpec,
+}
+
+pub struct DBSpec {
     pub database_url: String,
 }
 
-impl Config {
+pub struct WeatherStackSpec {
+    pub api_url: String,
+    pub api_key: String,
+}
+
+impl EnvConfig {
     pub fn from_env() -> Result<Self, env::VarError> {
-        Ok(Self {
+        let db_spec = DBSpec {
             database_url: env::var("DATABASE_URL")
-                .ok()
-                .unwrap_or("postgres://user:password@localhost:5432/pastcast".to_string()),
+                .unwrap_or_else(|_| "postgres://user:password@localhost:5432/pastcast".to_string()),
+        };
+
+        let weatherstack_spec = WeatherStackSpec {
+            api_key: env::var("WEATHER_STACK_API_KEY").unwrap_or_else(|_| "".to_string()),
+            api_url: env::var("WEATHER_STACK_API_URL")
+                .unwrap_or_else(|_| "https://api.weatherstack.com".to_string()),
+        };
+
+        Ok(Self {
+            db_spec,
+            weatherstack_spec,
         })
     }
 }
