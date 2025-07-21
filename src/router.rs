6bd5ccf4ed42592e39c3099c6941a3d5166e7492
Wynand Meintjes <wynandmeintjes@users.noreply.github.com>
2025-07-21 13:59:17 +0200

    feat: Adds logging tracing (#5)

--- src/router.rs
+++ src/router.rs
@@ -2,9 +2,17 @@ use crate::configs::state::AppState;
 use crate::handlers::weather_handlers::home_handler;
 use axum::{Router, routing::get};
 use std::sync::Arc;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn router_with_state(state: Arc<AppState>) -> Router {
+    // Set up logging with environment-based filter
+    let layer = tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     Router::new()
         .route("/", get(home_handler))
         .with_state(state)
+        .layer(layer)
 }
